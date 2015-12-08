c4fada068ee0aa37d33a10918c8827f9867cc771
s0rInb <s0rInb470@gmail.com>
2015-12-08 18:24:40 +0300

    change all

--- src/main/webapp/js/index.js
+++ src/main/webapp/js/index.js
@@ -3,11 +3,11 @@ $('#nav_ul').click(function (e) {
     e.preventDefault();
     $(this).tab('show');
 });
-$(function () {
-    //then load the JavaScript file
-    $.getScript('js/users.js');
-});
-$('a[href="#users"]').on("shown.bs.tab", function (e) {
-    w2ui.grid1.resize();
-    w2ui.grid2.resize();
-});
+//$(function () {
+//    //then load the JavaScript file
+//    $.getScript('js/users.js');
+//});
+//$('a[href="#users"]').on("shown.bs.tab", function (e) {
+//    w2ui.grid1.resize();
+//    w2ui.grid2.resize();
+//});
